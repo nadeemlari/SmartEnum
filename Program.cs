0e10c4a4ecaf018868ce899e277a29bdb3ab5605
Nadeem Lari <nadeemlari@gmail.com>
2021-12-02 20:22:59 +0530

    init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using SmartEnum;
+
+var free1  = Subscriptions.Free;
+var free2  = Subscriptions.Free;
+var free3 = Subscriptions.FromName("Free");
+var free4 = Subscriptions.FromValue(1);
+
+var vip = Subscriptions.Vip;
+Console.Write(free1==free2);
+Console.WriteLine($"Discount for {vip.Name} was {vip.Discount}");
---
+++ Subscriptions.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.SmartEnum;
+
+namespace SmartEnum;
+
+public sealed class Subscriptions: SmartEnum<Subscriptions>
+{
+    public double Discount { get; }
+    public static readonly Subscriptions Free = new("Free", 1,0);
+    public static readonly Subscriptions Premium = new("Premium", 2,.25);
+    public static readonly Subscriptions Vip = new("VIP", 3,.5);
+    public Subscriptions(string name, int value ,double discount) : base(name, value)
+    {
+        Discount = discount;
+    }
+}
